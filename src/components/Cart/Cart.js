47b5703465e5bb1a2beb62b9f9e2bba3385f504c
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-07-07 15:17:25 +0200

    feat: showing My Cart on click

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,7 +7,7 @@ const Cart = (props) => {
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
+        <CartItem 
           item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
         />
       </ul>
